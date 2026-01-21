5ab1e9b76ccd07e19d7fa3c85f2fa87eb55e365f
exy <pratyushtheone@gmail.com>
2026-01-21 23:59:05 +0530

    ..

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,40 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image } from "react-native";
+
+const Tabicon = ({
+  icon,
+  color,
+  name,
+  focused,
+}: {
+  icon: any;
+  color: string;
+  name: string;
+  focused: boolean;
+}) => {
+  return (
+    <view className="justify-center items-center gap-1">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-"
+      />
+    </view>
+  );
+};
 
 const _layout = () => {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <></>,
+        }}
       />
       <Tabs.Screen
         name="profile"
